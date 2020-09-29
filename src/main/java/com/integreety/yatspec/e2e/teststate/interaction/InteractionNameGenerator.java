69c4cd375d4e2ec5c56ed06290fef88f2d651338
Lukasz Gryzbon <lgryzbon@ford.com>
2020-09-29 09:37:02 +0100

    Include path in the report

--- src/main/java/com/integreety/yatspec/e2e/teststate/interaction/InteractionNameGenerator.java
+++ src/main/java/com/integreety/yatspec/e2e/teststate/interaction/InteractionNameGenerator.java
@@ -4,13 +4,17 @@ import com.integreety.yatspec.e2e.captor.repository.model.InterceptedCall;
 import com.integreety.yatspec.e2e.teststate.dto.Interaction;
 import com.integreety.yatspec.e2e.teststate.mapper.destination.DestinationNameMappings;
 import com.integreety.yatspec.e2e.teststate.mapper.source.SourceNameMappings;
+import de.vandermeer.asciitable.AT_Row;
 import de.vandermeer.asciitable.AsciiTable;
+import de.vandermeer.skb.interfaces.transformers.textformat.TextAlignment;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.tuple.Pair;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import static com.integreety.yatspec.e2e.teststate.indent.JsonPrettyPrinter.indentJson;
 import static com.integreety.yatspec.e2e.teststate.indent.XmlPrettyPrinter.indentXml;
@@ -20,12 +24,12 @@ import static com.integreety.yatspec.e2e.teststate.indent.XmlPrettyPrinter.inden
 public class InteractionNameGenerator {
 
     public List<Pair<String, Object>> generate(final SourceNameMappings sourceNameMappings, final DestinationNameMappings destinationNameMappings, final List<InterceptedCall> data) {
-        final List<List<String>> reportTable = new ArrayList<>();
+        final Set<List<String>> reportTable = new HashSet<>();
         final List<Pair<String, Object>> interactions = new ArrayList<>();
         for (final InterceptedCall interceptedCall : data) {
             final String destination = destinationNameMappings.mapForPath(interceptedCall.getTarget());
             final String source = sourceNameMappings.mapFor(Pair.of(interceptedCall.getServiceName(), interceptedCall.getTarget()));
-            reportTable.add(List.of(interceptedCall.getServiceName(), source, interceptedCall.getTarget(), destination));
+            reportTable.add(List.of(interceptedCall.getServiceName(), interceptedCall.getTarget(), source, interceptedCall.getTarget(), destination));
             final String interactionName = interceptedCall.getType().getInteractionName().apply(buildInteraction(interceptedCall, destination, source));
             log.info("Generated an interaction name={}", interactionName);
             final String body = indent(interceptedCall.getBody());
@@ -35,15 +39,18 @@ public class InteractionNameGenerator {
         return interactions;
     }
 
-    private void logReport(final List<List<String>> reportTable) {
+    private void logReport(final Set<List<String>> reportTable) {
         final AsciiTable at = new AsciiTable();
         at.addRule();
-        at.addRow(null, "Service name ---> Source", null, "Target ---> Destination");
+        final AT_Row row = at.addRow(null, null, "Service name & path ---> Source", null, "Target ---> Destination");
+        row.setTextAlignment(TextAlignment.CENTER);
         at.addRule();
         for(final List<String> reportTableRow: reportTable) {
             at.addRow(reportTableRow);
         }
         at.addRule();
+
+        at.getContext().setWidth(200);
         System.out.println(at.render());
     }
 
