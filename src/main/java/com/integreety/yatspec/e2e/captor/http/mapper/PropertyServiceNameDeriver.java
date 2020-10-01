3cb8381916fa24fd7321d76e14698a827751a080
Lukasz Gryzbon <lgryzbon@ford.com>
2020-10-02 00:57:02 +0100

    Comment added

--- src/main/java/com/integreety/yatspec/e2e/captor/http/mapper/PropertyServiceNameDeriver.java
+++ src/main/java/com/integreety/yatspec/e2e/captor/http/mapper/PropertyServiceNameDeriver.java
@@ -2,6 +2,7 @@ package com.integreety.yatspec.e2e.captor.http.mapper;
 
 public class PropertyServiceNameDeriver {
 
+    // TODO This should be configurable
     private static final String DEFAULT_SERVICE_NAME_SUFFIX_TO_REMOVE = " Service";
 
     private final String appName;
