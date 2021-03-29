510549fc68b33e46ee27652829d848caab3e60f3
Lukasz Gryzbon <lgryzbon@ford.com>
2021-03-29 13:04:21 +0200

    Resolve a missing AMQP target header to UNKNOWN_EVENT

--- src/main/java/com/integreety/yatspec/e2e/captor/rabbit/mapper/ExchangeNameDeriver.java
+++ src/main/java/com/integreety/yatspec/e2e/captor/rabbit/mapper/ExchangeNameDeriver.java
@@ -4,29 +4,34 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.core.MessageProperties;
 
 import java.util.Arrays;
+import java.util.Optional;
 
 import static org.apache.commons.lang3.StringUtils.isBlank;
 
 @Slf4j
 public class ExchangeNameDeriver {
 
-    // TODO This method of determining the exchange name is not reliable
     private static final String TYPE_ID_HEADER = "__TypeId__";
+    private static final String TARGET_NAME_KEY = "Target-Name";
+    private static final String UNKNOWN_EVENT = "UNKNOWN_EVENT";
 
-    public String derive(final MessageProperties messageProperties, final String defaultExchangeName)  {
-        if (messageProperties.getHeader(TYPE_ID_HEADER) == null) {
-            return defaultExchangeName;
+    public String derive(final MessageProperties messageProperties, final String alternativeExchangeName)  {
+        final String defaultExchangeName = getDefaultExchangeName(alternativeExchangeName);
+        if (!isBlank(messageProperties.getHeader(TARGET_NAME_KEY))) {
+            return messageProperties.getHeader(TARGET_NAME_KEY);
         }
-        final String exchangeName = deriveFromMessageProperties(messageProperties);
-        if (!isBlank(exchangeName)) {
-            return exchangeName;
+        if (!isBlank(messageProperties.getHeader(TYPE_ID_HEADER))) {
+            return deriveFromTypeIdHeader(messageProperties.getHeader(TYPE_ID_HEADER)).orElse(defaultExchangeName);
         }
         return defaultExchangeName;
     }
 
-    private String deriveFromMessageProperties(final MessageProperties messageProperties) {
-        return Arrays.stream(messageProperties.getHeader(TYPE_ID_HEADER).toString().split("\\."))
-                .reduce((first, second) -> second)
-                .orElse(null);
+    private String getDefaultExchangeName(final String alternativeExchangeName) {
+        return !isBlank(alternativeExchangeName) ? alternativeExchangeName : UNKNOWN_EVENT;
+    }
+
+    private Optional<String> deriveFromTypeIdHeader(final String typeIdHeader) {
+        return Arrays.stream(typeIdHeader.split("\\."))
+                .reduce((first, second) -> second);
     }
 }
