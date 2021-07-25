055b862cc2e6f3d5ee6264b0f34440056b054ba2
lukasz.gryzbon <lukasz.gryzbon@integreety.co.uk>
2021-07-26 00:14:05 +0100

    #18 Integration tests refactored and updated

--- src/test/java/io/lsdconsulting/lsd/distributed/integration/InterceptedDocumentMongoRepositoryIT.java
+++ src/test/java/io/lsdconsulting/lsd/distributed/integration/InterceptedDocumentMongoRepositoryIT.java
@@ -2,12 +2,14 @@ package io.lsdconsulting.lsd.distributed.integration;
 
 import com.mongodb.client.ListIndexesIterable;
 import io.lsdconsulting.lsd.distributed.captor.repository.mongo.InterceptedDocumentMongoRepository;
+import io.lsdconsulting.lsd.distributed.integration.testapp.TestApplication;
+import io.lsdconsulting.lsd.distributed.integration.testapp.config.RepositoryConfig;
 import io.lsdconsulting.lsd.distributed.integration.testapp.repository.TestRepository;
-import lombok.extern.slf4j.Slf4j;
 import org.bson.Document;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -16,21 +18,14 @@ import static java.util.stream.StreamSupport.stream;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.hasItem;
 import static org.hamcrest.Matchers.startsWith;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
-@Slf4j
+@SpringJUnitConfig(classes = {RepositoryConfig.class})
+@SpringBootTest(webEnvironment = RANDOM_PORT, classes = {TestApplication.class})
+@AutoConfigureWireMock(port = 0)
 class InterceptedDocumentMongoRepositoryIT {
 
     private final TestRepository testRepository = new TestRepository();
-
-    @BeforeAll
-    public static void setup() {
-        TestRepository.setupDatabase();
-    }
-
-    @AfterAll
-    public static void tearDown() {
-        TestRepository.tearDownDatabase();
-    }
 
     @Test
     public void shouldCreateCollectionWithIndexes() {
