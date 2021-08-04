20dba5d4b6c005d4a941f4475795884dbfe84fc2
lukasz.gryzbon <lukasz.gryzbon@integreety.co.uk>
2021-08-04 22:59:23 +0100

    #23 Make responses synchronous explicitly.

--- src/test/java/io/lsdconsulting/lsd/distributed/integration/EndToEndIT.java
+++ src/test/java/io/lsdconsulting/lsd/distributed/integration/EndToEndIT.java
@@ -91,15 +91,15 @@ public class EndToEndIT extends IntegrationTestBase {
         assertThat(report, containsString("publish event"));
         assertThat(report, containsString("SomethingDoneEvent -&gt; TestApp"));
         assertThat(report, containsString("consume message"));
-        assertThat(report, containsString("TestApp -&gt; Client"));
+        assertThat(report, containsString("TestApp --&gt;&gt; Client"));
         assertThat(report, containsString("200 OK response"));
         assertThat(report, containsString("TestApp -&gt; UNKNOWN_TARGET"));
         assertThat(report, containsString("POST /external-api?message=from_feign"));
-        assertThat(report, containsString("UNKNOWN_TARGET -&gt; TestApp"));
+        assertThat(report, containsString("UNKNOWN_TARGET --&gt;&gt; TestApp"));
         assertThat(report, containsString("200 OK response"));
         assertThat(report, containsString("TestApp -&gt; Downstream"));
         assertThat(report, containsString("POST /external-api?message=from_feign"));
-        assertThat(report, containsString("Downstream -&gt; TestApp"));
+        assertThat(report, containsString("Downstream --&gt;&gt; TestApp"));
         assertThat(report, containsString("200 OK response"));
     }
 
