ba325c174761cb747dd14d031dd6b735d4533069
Lukasz Gryzbon <lukasz.gryzbon@gmail.com>
2023-05-05 17:36:55 +0100

    refactor: Convert to Kotlin: AmqpHeaderRetriever

---
+++ src/main/java/io/lsdconsulting/lsd/distributed/interceptor/captor/rabbit/AmqpHeaderRetriever.kt
@@ -0,0 +1,17 @@
+package io.lsdconsulting.lsd.distributed.interceptor.captor.rabbit
+
+import io.lsdconsulting.lsd.distributed.interceptor.captor.header.Obfuscator
+import lombok.RequiredArgsConstructor
+import org.springframework.amqp.core.Message
+
+@RequiredArgsConstructor
+class AmqpHeaderRetriever(
+    private val obfuscator: Obfuscator
+) {
+    fun retrieve(message: Message): Map<String, Collection<String>> =
+        obfuscator.obfuscate(
+            message.messageProperties.headers.entries.associate {
+                it.key to (it.value?.let { _ -> listOf(it.value.toString()) } ?: emptyList())
+            }
+        )
+}
