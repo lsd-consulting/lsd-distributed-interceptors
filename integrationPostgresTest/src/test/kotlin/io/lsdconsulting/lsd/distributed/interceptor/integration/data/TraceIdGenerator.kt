76f1c63b4931d294e7c6db321127eaed94c790e0
Lukasz Gryzbon <lukasz.gryzbon@gmail.com>
2024-02-09 00:07:32 +0000

    chore: #57 Complete upgrade to Spring Boot 3
    BREAKING CHANGE: Major Spring Boot version upgrade

--- integrationPostgresTest/src/test/kotlin/io/lsdconsulting/lsd/distributed/interceptor/integration/data/TraceIdGenerator.kt
+++ integrationPostgresTest/src/test/kotlin/io/lsdconsulting/lsd/distributed/interceptor/integration/data/TraceIdGenerator.kt
@@ -1,13 +1,13 @@
 package io.lsdconsulting.lsd.distributed.interceptor.integration.data
 
 import lsd.logging.log
-import org.apache.commons.lang3.RandomUtils
+import java.util.*
 
 object TraceIdGenerator {
     private const val MIN_VALID_TRACE_ID_VALUE = 1152921504606846976L
 
     fun generate(): String {
-        val traceId = java.lang.Long.toHexString(RandomUtils.nextLong(MIN_VALID_TRACE_ID_VALUE, Long.MAX_VALUE))
+        val traceId = java.lang.Long.toHexString(Random().nextLong(MIN_VALID_TRACE_ID_VALUE, Long.MAX_VALUE))
         log().info("Generated new traceId:{}", traceId)
         return traceId
     }
