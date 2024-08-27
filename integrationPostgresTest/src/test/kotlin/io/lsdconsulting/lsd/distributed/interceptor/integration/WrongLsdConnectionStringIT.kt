ca732d66a9d951bc71565d148492292feefb8c9c
Lukasz Gryzbon <lukasz.gryzbon@gmail.com>
2024-08-27 08:14:02 +0100

    build: Bump Gradle version to 8.10

--- integrationPostgresTest/src/test/kotlin/io/lsdconsulting/lsd/distributed/interceptor/integration/WrongLsdConnectionStringIT.kt
+++ integrationPostgresTest/src/test/kotlin/io/lsdconsulting/lsd/distributed/interceptor/integration/WrongLsdConnectionStringIT.kt
@@ -1,10 +1,8 @@
 package io.lsdconsulting.lsd.distributed.interceptor.integration
 
-import org.hamcrest.Matchers.*
 import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 import org.springframework.test.context.ActiveProfiles
-import java.util.*
 
 @ActiveProfiles("wrong-connection-string")
 @Disabled // TODO Haven't figured out how to assert when app fails to start
