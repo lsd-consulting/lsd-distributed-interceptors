5cb0d71f4ac90305cc472f60bfcb637e9404ba5e
Nick McDowall <nicholasinengland@hotmail.com>
2025-10-28 15:18:52 +0000

    chore(deps): bump flapdoodle

--- integrationMongoTest/src/test/kotlin/io/lsdconsulting/lsd/distributed/interceptor/integration/testapp/repository/TestRepository.kt
+++ integrationMongoTest/src/test/kotlin/io/lsdconsulting/lsd/distributed/interceptor/integration/testapp/repository/TestRepository.kt
@@ -6,12 +6,12 @@ import com.mongodb.MongoException
 import com.mongodb.client.MongoClient
 import com.mongodb.client.MongoClients
 import com.mongodb.client.model.Filters
-import de.flapdoodle.embed.mongo.MongodExecutable
-import de.flapdoodle.embed.mongo.MongodStarter
-import de.flapdoodle.embed.mongo.config.MongodConfig
 import de.flapdoodle.embed.mongo.config.Net
 import de.flapdoodle.embed.mongo.distribution.Version
-import de.flapdoodle.embed.process.runtime.Network
+import de.flapdoodle.embed.mongo.transitions.Mongod
+import de.flapdoodle.embed.mongo.transitions.RunningMongodProcess
+import de.flapdoodle.reverse.TransitionWalker.ReachedState
+import de.flapdoodle.reverse.transitions.Start
 import io.lsdconsulting.lsd.distributed.connector.model.InteractionType
 import io.lsdconsulting.lsd.distributed.connector.model.InterceptedInteraction
 import io.lsdconsulting.lsd.distributed.mongo.repository.codec.ZonedDateTimeCodec
@@ -33,7 +33,7 @@ class TestRepository {
 
     fun findAll(traceId: String): List<InterceptedInteraction> {
         log().info("Retrieving interceptedInteractions for traceId:{}", traceId)
-        val database = mongoClient!!.getDatabase(DATABASE_NAME)
+        val database = mongoClient.getDatabase(DATABASE_NAME)
         val collection = database.getCollection(COLLECTION_NAME).withCodecRegistry(pojoCodecRegistry)
         val result: MutableList<InterceptedInteraction> = ArrayList()
         try {
@@ -84,16 +84,15 @@ class TestRepository {
         const val MONGODB_PORT = 27017
         private const val DATABASE_NAME = "lsd"
         private const val COLLECTION_NAME = "interceptedInteraction"
-        private var mongoClient: MongoClient? = null
-        private var mongodExecutable: MongodExecutable? = null
+        private lateinit var mongoClient: MongoClient
+        private lateinit var mongodExecutable: ReachedState<RunningMongodProcess>
         fun setupDatabase() {
             try {
-                val mongodConfig: MongodConfig = MongodConfig.builder()
-                    .version(Version.Main.V5_0)
-                    .net(Net(MONGODB_HOST, MONGODB_PORT, Network.localhostIsIPv6()))
-                    .build()
-                mongodExecutable = MongodStarter.getDefaultInstance().prepare(mongodConfig)
-                mongodExecutable!!.start()
+                val mongod = Mongod.builder()
+                    .net(
+                        Start.to(Net::class.java).initializedWith(Net.defaults().withPort(MONGODB_PORT))
+                    ).build()
+                mongodExecutable = mongod.start(Version.Main.V5_0)
                 mongoClient = MongoClients.create(
                     MongoClientSettings.builder()
                         .applyConnectionString(ConnectionString("mongodb://" + MONGODB_HOST + ":" + MONGODB_PORT))
@@ -106,8 +105,11 @@ class TestRepository {
         }
 
         fun tearDownDatabase() {
-            mongoClient!!.close()
-            mongodExecutable!!.stop()
+            mongoClient.close()
+            runCatching {
+                mongodExecutable.close()
+            }.onFailure { println("Exception caught while tearing down database (might already be down): ${it.message}") }
+
         }
     }
 }
